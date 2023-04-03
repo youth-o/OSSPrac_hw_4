3f567bab09b23efd9cde2be41c4c96768313bab4
Youth-o <2021111495@dgu.ac.kr>
2023-04-04 00:41:09 +0900

    2023-04-03 실습#4 5번

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 name = input("이름을 입력하세요: ")
 num = int(input("학번을 입력하세요: "))
 course = input("학과를 입력하세요: ")
+grade = int(input("학년을 입력하세요: "))
 
 print("<출력>")
-print("이름: ", name, "\n학번: ", num, "\n학과: ", course)
+print("이름: ", name, "\n학번: ", num, "\n학과: ", course, "\n학년: ", grade)
